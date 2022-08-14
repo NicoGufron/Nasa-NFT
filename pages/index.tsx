5d42507f2ae8872f5c99b5e293fc8736aea9e623
Nico Gufron <nicogufron@gmail.com>
2022-08-14 21:22:29 +0700

    Added images

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,25 +61,25 @@ const Home: NextPage = () => {
                 <img className='marquee-pictures' src="/images/3.png"></img>
                 <img className='marquee-pictures' src="/images/4.png"></img>
                 <img className='marquee-pictures' src="/images/5.png"></img>
-                <img className='marquee-pictures' src="/images/1.png"></img>
+                <img className='marquee-pictures' src="/images/6.png"></img>
               </Marquee>
 
               <Marquee gradient={false} direction={"left"}>
-                <img className='marquee-pictures' src="/images/1.png"></img>
-                <img className='marquee-pictures' src="/images/2.png"></img>
-                <img className='marquee-pictures' src="/images/3.png"></img>
-                <img className='marquee-pictures' src="/images/4.png"></img>
-                <img className='marquee-pictures' src="/images/5.png"></img>
-                <img className='marquee-pictures' src="/images/1.png"></img>
+                <img className='marquee-pictures' src="/images/7.png"></img>
+                <img className='marquee-pictures' src="/images/8.png"></img>
+                <img className='marquee-pictures' src="/images/9.png"></img>
+                <img className='marquee-pictures' src="/images/10.png"></img>
+                <img className='marquee-pictures' src="/images/11.png"></img>
+                <img className='marquee-pictures' src="/images/12.png"></img>
               </Marquee>
 
               <Marquee gradient={false} direction={"right"}>
-                <img className='marquee-pictures' src="/images/1.png"></img>
-                <img className='marquee-pictures' src="/images/2.png"></img>
-                <img className='marquee-pictures' src="/images/3.png"></img>
-                <img className='marquee-pictures' src="/images/4.png"></img>
-                <img className='marquee-pictures' src="/images/5.png"></img>
-                <img className='marquee-pictures' src="/images/1.png"></img>
+                <img className='marquee-pictures' src="/images/13.png"></img>
+                <img className='marquee-pictures' src="/images/14.png"></img>
+                <img className='marquee-pictures' src="/images/15.png"></img>
+                <img className='marquee-pictures' src="/images/16.png"></img>
+                <img className='marquee-pictures' src="/images/17.png"></img>
+                <img className='marquee-pictures' src="/images/18.png"></img>
               </Marquee>
             </div>
           </div>
