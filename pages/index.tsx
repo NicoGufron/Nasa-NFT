c04bfa49e119d3d83cf9b6ee35fa723cacfa018b
Nico Gufron <nicogufron@gmail.com>
2022-08-15 01:59:39 +0700

    Added home

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='navbar'>
         <ul className='nav'>
+        <li className='nav-links'><a href="#">Home</a></li>
           <li className='nav-links'><a href="#collection">Collection</a></li>
           <li className='nav-links'><a href="#about">About</a></li>
           <li className='nav-links'><a href="#network">Network</a></li>
