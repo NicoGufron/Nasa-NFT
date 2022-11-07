f0b7254e140c69100f287d3350a2355f50dbfdfa
NicoGuff <nicogufron@gmail.com>
2022-11-07 14:58:10 +0800

    Changed few lines

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -11,18 +11,18 @@ const FAQ: NextPage = () => {
             "How many token supply will be created?",
             "Are there any taxes?",
             "What network are you invading?",
-            "Will be the contract be shared before started trading?",
+            "Will the contract be shared before started trading?",
             "Are there any NFTs will be launched?",
-            "Why there is a mystery box above FAQ?"
+            "What is mystery box?"
         ],
         answers: [
-            "National Aeronautics and Space Administration for human language. But actually it is Not A Shitty Alien.",
-            "1.000.000.000 tokens will be created.",
-            "Yes, there are taxes will be applied, such as marketing and liquidity taxes. It will be 5 / 5.",
-            "Using our latest technology of sniping, we set our sights in Ethereum.",
-            "Of course. We want the trust to be mutual. Humans will be able to check our contract before the mint phase start.",
+            "National Aeronautics and Space Administration for human language. Pfftt lol",
+            "1000 x 10⁶ tokens will be generated.",
+            "Yes, there will be taxes. 1% for Liquidity and 4% for our Military Funds.",
+            "Ethereum will be nicer than BSC. The shitty people are strong here.",
+            "No. Those shitty people will fire at us immediately.",
             "Yes! We have NFTs that will be launched soon enough when it is ready!",
-            "Huh? What mystery box?"
+            "What mystery box?"
         ]
     }
 
